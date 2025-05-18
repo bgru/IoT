6277c015eef30f8487cb3bdce0a8fad914938a21
finny <bbgruda223@gmail.com>
2025-05-18 11:33:54 +0200

    Configure Functions app startup and host settings

---
+++ IndustrialIoT.Functions/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(IndustrialIoT.Functions.Startup))]
+
+namespace IndustrialIoT.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Register Azure IoT Service
+            builder.Services.AddSingleton<IAzureIoTService, AzureIoTService>();
+        }
+    }
+}
